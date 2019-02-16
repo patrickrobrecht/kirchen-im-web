0ee4ced0fbe7b0e2bc539350db87d526dd21608f
Patrick Robrecht <patrickrobrecht@users.noreply.github.com>
2019-02-16 13:49:58 +0000

    Email notifications whenever a church is added; release as version 3.5 (#23)
    * Simple notification whenever a church is added
    
    * Disable debugging, fix encoding issues

---
+++ src/Helpers/Mailer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace KirchenImWeb\Helpers;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mailer extends AbstractHelper
+{
+    public function sendMail($subject, $bodyHTML, $bodyText = '')
+    {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = MAIL_HOST;
+        $mail->Port = 25;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->Username = MAIL_USERNAME;
+        $mail->Password = MAIL_PASSWORD;
+
+        $mail->setFrom(MAIL_FROM);
+        $mail->addAddress(MAIL_TO);
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = $subject;
+        $mail->msgHTML($bodyHTML);
+        $mail->AltBody = $bodyText;
+
+        try {
+            return $mail->send();
+        } catch (Exception $exception) {
+            return false;
+        }
+    }
+}
